04b0ae189239b204bcb8fbc5b398a3584cb9fa84
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-31 08:43:48 +0200

    Correctly dispose McpClient on Quit, CTRL-C and Application exit.

---
+++ src/mcpclient/Extensions/McpClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace ModelContextProtocol.Client.Extensions;
+
+internal static class McpClientExtensions
+{
+    public static void DisposeAsyncOnApplicationExit(this IMcpClient client)
+    {
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            e.Cancel = false;
+            DisposeAsync(client);
+        };
+
+        AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+        {
+            DisposeAsync(client);
+        };
+    }
+
+    private static void DisposeAsync(IMcpClient client)
+    {
+        client.DisposeAsync().GetAwaiter().GetResult();
+        Thread.Sleep(100);
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
