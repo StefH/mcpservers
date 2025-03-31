07a483cbc18fb1906ddd2d029f8290d1f5dad119
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-31 19:07:25 +0200

    ...

--- tests/mcpserver.azuredevops.stdio.ConsoleApp/Program.cs
+++ tests/mcpserver.azuredevops.stdio.ConsoleApp/Program.cs
@@ -1,10 +1,19 @@
-﻿//using Microsoft.Extensions.Configuration;
-//using ModelContextProtocolServer.AzureDevops.Stdio.Services;
-//using ModelContextProtocolServer.AzureDevops.Stdio.Tools;
+﻿using Microsoft.Extensions.Configuration;
+using ModelContextProtocolServer.AzureDevops.Stdio.Services;
+using ModelContextProtocolServer.AzureDevops.Stdio.Tools;
 
-//var azureDevOpsClient = new AzureDevOpsClient();
+var configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(new List<KeyValuePair<string, string?>>
+    {
+        new("AZURE_DEVOPS_ORG_URL", "https://dev.azure.com/mstack"),
+        new("AZURE_DEVOPS_PAT", Environment.GetEnvironmentVariable("MCP_PAT"))
+    })
+    .AddEnvironmentVariables()
+    .Build();
 
-//var projectsTools = new ProjectsTools(azureDevOpsClient);
-//var result = await projectsTools.GetProjects(4, null);
+var azureDevOpsClient = new AzureDevOpsClient(configuration);
 
-//int x = 0;
+var projectsTools = new ProjectsTools(azureDevOpsClient);
+var result = await projectsTools.GetProjects(5);
+
+Console.WriteLine(result);
