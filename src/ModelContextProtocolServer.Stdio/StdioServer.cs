2b63b931128b93a01693fc8deb992240ac55aecf
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-04-04 20:06:33 +0200

    remove unused using

--- src/ModelContextProtocolServer.Stdio/StdioServer.cs
+++ src/ModelContextProtocolServer.Stdio/StdioServer.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ModelContextProtocol;
 using ModelContextProtocol.Protocol.Types;
 
 namespace ModelContextProtocolServer.Stdio;
