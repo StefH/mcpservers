a218d61ce39db290b9b62f3feb854a50a2576098
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-02-01 12:19:30 +0100

    ok

---
+++ example/ModelContextProtocol-SemanticKernel.Stdio.Example/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.SemanticKernel.Extensions;
+
+var currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+using var cts = new CancellationTokenSource();
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddLogging(c => c.AddDebug().SetMinimumLevel(LogLevel.Trace));
+
+builder.Services.AddOpenAIChatCompletion(
+    serviceId: "openai",
+    modelId: "gpt-4o",
+    apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY")!
+);
+
+var kernel = builder.Build();
+
+await kernel.Plugins.AddMcpFunctionsFromStdioServerAsync(
+    "MCPInterceptor for Everything",
+    "dotnet run --project",
+    [
+        @"C:\dev\GitHub\mcpservers\src\mcpserver.interceptor\mcpserver.interceptor.csproj",
+        //"--",
+        //"--serverName=Everything",
+        //"--command=\"dnx --yes mcpserver.everything.stdio\""
+    ],
+    cancellationToken: cts.Token);
+
+await kernel.Plugins.AddMcpFunctionsFromStdioServerAsync(
+    "MCPInterceptor for AzureDevOps",
+    "dotnet run --project",
+    [@"C:\dev\GitHub\mcpserver.azuredevops\src\mcpserver.azuredevops.stdio\mcpserver.azuredevops.stdio.csproj"],
+    new Dictionary<string, string>
+    {
+        { "AZURE_DEVOPS_ORG_URL", "https://dev.azure.com/alfa1group" },
+        { "AZURE_DEVOPS_AUTH_METHOD", "pat" },
+        { "AZURE_DEVOPS_PAT", Environment.GetEnvironmentVariable("AZURE_DEVOPS_PAT")! }
+    },
+    cancellationToken: cts.Token);
+
+var executionSettings = new OpenAIPromptExecutionSettings
+{
+    Temperature = 0.1,
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+var logger = kernel.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Program");
+
+//var result = await kernel.InvokePromptAsync("Which tools are currently registered? And what are the functions?", new(executionSettings));
+//Console.WriteLine($"\n\nTools:\n{result}");
+
+//var promptComplex = "Use the Everything tool and call the add_complex function to add these complex numbers: 1 + 2i and 3 - 7i";
+//var resultComplex = await kernel.InvokePromptAsync(promptComplex, new(executionSettings));
+//Console.WriteLine($"\n\n{promptComplex}\n{resultComplex}");
+
+var promptAzureDevops =
+    """
+    For the Azure Devops project 'mstack-skills' and repository 'mstack-skills-blazor', get 2 latest commits with all details.
+    """;
+var resultAzureDevops = await kernel.InvokePromptAsync(promptAzureDevops, new(executionSettings));
+Console.WriteLine($"\n\n{promptAzureDevops}\n{resultAzureDevops}");
+
+await cts.CancelAsync();
---
+++ src/mcpserver.interceptor/McpInterceptorCommand.cs
@@ -0,0 +1,171 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using ModelContextProtocol;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+using ModelContextProtocolServer.Stdio;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace ModelContextProtocolServer.Interceptor;
+
+internal sealed class McpInterceptorCommand : AsyncCommand<McpInterceptorCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [CommandOption("--logger")]
+        [Description("Logger (console or null)")]
+        [DefaultValue("console")]
+        public required string Logger { get; init; }
+
+        [CommandOption("-s|--serverName", true)]
+        [Description("MCP Server Name")]
+        public required string Servername { get; init; }
+
+        [CommandOption("-c|--command")]
+        [Description("MCP Server Command with Arguments (stdio only)")]
+        public required string CommandWithArguments { get; init; }
+
+        public string Command { get; private set; } = null!;
+
+        public string[] Arguments { get; private set; } = [];
+
+        public Dictionary<string, string?> EnvironmentVariables { get; private set; } = [];
+
+        public override ValidationResult Validate()
+        {
+            if (Logger != "console" && Logger != "null")
+            {
+                return ValidationResult.Error("The logger should be 'console' or 'null'.");
+            }
+
+            if (string.IsNullOrWhiteSpace(Servername))
+            {
+                return ValidationResult.Error("The servername is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(CommandWithArguments))
+            {
+                return ValidationResult.Error("The command is required for stdio.");
+            }
+
+            var commandWithArguments = CommandWithArguments.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Command = commandWithArguments[0];
+
+            if (commandWithArguments.Length > 1)
+            {
+                var arguments = commandWithArguments.Skip(1).Where(a => !a.StartsWith("env:")).ToArray();
+                if (arguments.Length > 0)
+                {
+                    Arguments = arguments;
+                }
+
+                var environmentVariables = commandWithArguments.Skip(1).Where(a => a.StartsWith("env:")).ToArray();
+                foreach (var e in environmentVariables.Select(e => e.Split("=")).Select(x => new { name = x[0], value = x[1] }))
+                {
+                    EnvironmentVariables[e.name] = e.value;
+                }
+            }
+
+            //if (!System.Diagnostics.Debugger.IsAttached)
+            //{
+            //    System.Diagnostics.Debugger.Launch();
+            //}
+
+            return ValidationResult.Success();
+        }
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    {
+        var assembly = Assembly.GetEntryAssembly();
+        var assemblyTitle = assembly?.GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? $"mcpserver.{Guid.NewGuid()}.stdio";
+        var name = $"{assemblyTitle} (interceptor-for-{settings.Servername})";
+        var version = assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+')[0] ?? "1.0.0";
+
+        var builder = Host.CreateEmptyApplicationBuilder(settings: new HostApplicationBuilderSettings
+        {
+            ApplicationName = name,
+            Args = settings.Arguments
+        });
+
+        var stdioOptions = new StdioClientTransportOptions
+        {
+            Command = settings.Command,
+            Arguments = settings.Arguments,
+            EnvironmentVariables = settings.EnvironmentVariables,
+            Name = name
+        };
+
+        var clientTransport = new StdioClientTransport(stdioOptions, null);
+
+        var clientOptions = new McpClientOptions
+        {
+            ClientInfo = new()
+            {
+                Name = name,
+                Version = version
+            }
+        };
+
+        var loggerFactory = LoggerHelper.CreateLoggerFactory(name);
+
+        var client = await McpClient.CreateAsync(clientTransport, clientOptions, loggerFactory: loggerFactory, cancellationToken: cancellationToken);
+
+        builder.Configuration
+            .AddCommandLine(settings.Arguments)
+            .AddEnvironmentVariables();
+
+        builder.Services
+            .AddSingleton(loggerFactory)
+            .AddMcpServer(async o =>
+            {
+                o.ServerInfo = new Implementation
+                {
+                    Name = name,
+                    Version = version
+                };
+
+                o.Handlers = new McpServerHandlers
+                {
+                    ListToolsHandler = async (request, cancellationToken) =>
+                    {
+                        var tools = await client.ListToolsAsync(cancellationToken: cancellationToken);
+
+                        return new ListToolsResult
+                        {
+                            Tools = tools.Select(t => new Tool
+                            {
+                                Name = t.Name,
+                                Title = t.Title,
+                                Description = t.Description,
+                                InputSchema = t.JsonSchema,
+                                OutputSchema = t.ReturnJsonSchema
+                            }).ToList()
+                        };
+                    },
+
+                    CallToolHandler = async (request, cancellationToken) =>
+                    {
+                        if (request.Params?.Name == null)
+                        {
+                            throw new McpProtocolException("Missing required parameter 'name'", McpErrorCode.InvalidParams);
+                        }
+
+                        return await client.CallToolAsync(request.Params, cancellationToken);
+                    }
+                };
+            }
+        )
+        .WithStdioServerTransport();
+
+        var host = builder.Build();
+
+        await host.RunAsync(cancellationToken);
+
+        await client.DisposeAsync();
+
+        return 0;
+    }
+}
